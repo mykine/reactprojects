f7f2d44cbd2912f1d97bcd87fce50192b8825b15
mykine <mykine@sina.com>
2019-12-26 17:16:12 +0800

    页面架构样式需配置less

--- react-demos-master/bikemanager/src/admin.js
+++ react-demos-master/bikemanager/src/admin.js
@@ -4,18 +4,19 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
 import 'antd/dist/antd.css'
+import './style/common.less'
 
 export default class Admin extends React.Component{
     
     render(){
         return (
-            <Row>
-                <Col span="3">
+            <Row className="container">
+                <Col span="4" className="nav-left">
                     <NavLeft/>
                 </Col>
-                <Col span="21">
+                <Col span="20" className="main">
                     <Header/>
-                    <Row>操作区</Row>
+                    <Row className="content" className="content">操作区</Row>
                     <Footer/>
                 </Col>
             </Row>
