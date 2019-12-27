7ab7ecf26fe5111205cc6975d1c907baefea3b3f
mykine <mykine@sina.com>
2019-12-27 18:09:12 +0800

    home页面手动填充到admin的操作区

--- react-demos-master/bikemanager/src/admin.js
+++ react-demos-master/bikemanager/src/admin.js
@@ -3,6 +3,7 @@ import { Row,Col } from 'antd'//导入栅格组件
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import Home from './pages/home'
 import 'antd/dist/antd.css'
 import './style/common.less'
 
@@ -16,7 +17,9 @@ export default class Admin extends React.Component{
                 </Col>
                 <Col span={20} className="main">
                     <Header/>
-                    <Row className="content" className="content">操作区</Row>
+                    <Row className="content" className="content">
+                       <Home />
+                    </Row>
                     <Footer/>
                 </Col>
             </Row>
---
+++ react-demos-master/bikemanager/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+
+export default class Home extends React.Component{
+    render(){
+        return (
+            <div className="home-wrap">
+                好好学习，天天向上~
+            </div>
+        )
+    }
+}
