f62029291f2dbdb7d104bac96b33981bc6658cb6
mykine <mykine@sina.com>
2019-12-27 11:57:39 +0800

    菜单显示demo

--- react-demos-master/bikemanager/src/components/NavLeft/index.js
+++ react-demos-master/bikemanager/src/components/NavLeft/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import MenuConfig from './../../config/menuConfig'
+import { Menu, Icon } from 'antd'
+import './index.less'
+const { SubMenu } = Menu
+
+
 export default class NavLeft extends React.Component{
     render(){
         return (
-            <div style={{backgroundColor:'red'}}>
-               左边菜单导航
+            <div>
+               <div className="logo">
+                    <img src="/assets/logo-ant.svg"></img>
+                    <h1>管理系统</h1>
+               </div>
+               <Menu theme="dark">
+                <SubMenu key="sub1" 
+                         title={
+                            <span>
+                            <Icon type="mail" />
+                            <span>Navigation One</span>
+                            </span>
+                          }
+                >
+                    <Menu.Item key="1">Option 1</Menu.Item>
+                    <Menu.Item key="2">Option 2</Menu.Item>
+                </SubMenu>
+               </Menu>
             </div>
         );
     }
---
+++ react-demos-master/bikemanager/src/config/menuConfig.js
@@ -0,0 +1,21 @@
+const menuList = [
+    {
+        title:"首页",
+        key:"/home"
+    },
+    {
+        title:"UI",
+        key:"/ui",
+        children:[
+            {
+                title:"按钮",
+                key:"/ui/buttons"
+            },
+            {
+                title:"弹窗",
+                key:"/ui/modals"
+            },
+        ]
+    },
+];
+export default menuList;
