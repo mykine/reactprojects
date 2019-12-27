7ebdbd769e8406b82014a9b099c97c6f6d226d5c
mykine <mykine@sina.com>
2019-12-27 17:47:33 +0800

    底部样式ok

--- react-demos-master/bikemanager/src/components/Footer/index.js
+++ react-demos-master/bikemanager/src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './index.less'
 export default class Footer extends React.Component{
     render(){
         return (
-            <div>
-                底部Footer
+            <div className="footer">
+                版权所有：开心幼稚园（推荐使用谷歌浏览器，可以获得更佳操作页面体验） 技术支持：小学僧
             </div>
         );
     }
