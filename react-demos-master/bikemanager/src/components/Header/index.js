747fce908f7adf933b68096ce9c8e442c30185a9
mykine <mykine@sina.com>
2019-12-27 15:07:23 +0800

    页面头部显示 `用户名，退出按钮，面包屑位置`

--- react-demos-master/bikemanager/src/components/Header/index.js
+++ react-demos-master/bikemanager/src/components/Header/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Row,Col } from 'antd'
 
+import './index.less'
 export default class Header extends React.Component{
+
+    componentWillMount(){
+        const username = "小学僧";
+        this.setState({
+            username
+        })
+    }
     render(){
         return (
-            <div>
-                头部Header
+            <div className="header">
+                <Row className="header-top">
+                    <Col span={24}>
+                        <span>欢迎，{this.state.username}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    首页>
+                </Row>
             </div>
         );
     }
